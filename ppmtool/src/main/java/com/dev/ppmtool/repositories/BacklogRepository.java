7d6bf1e1a32d527194b514e3596e66035af9d39e
mcortz <mykortz@gmail.com>
2019-02-12 22:54:47 +0800

    backlog and project task springboot

---
+++ ppmtool/src/main/java/com/dev/ppmtool/repositories/BacklogRepository.java
@@ -0,0 +1,10 @@
+package com.dev.ppmtool.repositories;
+
+import com.dev.ppmtool.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+
+}
