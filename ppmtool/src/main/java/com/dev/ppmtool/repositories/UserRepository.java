cb25c00c43024c7bb99c0a72d9150f1247d9f17b
mcortz <mykortz@gmail.com>
2019-02-27 23:51:59 +0800

    create user object, validation, repository, service

---
+++ ppmtool/src/main/java/com/dev/ppmtool/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.dev.ppmtool.repositories;
+
+import com.dev.ppmtool.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
